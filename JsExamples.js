a10f1c147e7c865be7004438b50e5b3ed2bfc63f
iphiljobs <43407650+iphiljobs@users.noreply.github.com>
2018-09-21 16:12:39 +0530

    Update JsExamples.js

--- JsExamples.js
+++ JsExamples.js
@@ -1,15 +1,17 @@
 //CRUD Operations:
 //1. ARRAYS:
-let arr = new Array ();
-let arr = [];
-let arr = [‘1’,’A’,’a’];
-//a.ADDING DATA	
-arr.push(‘x’);			//@end		[‘1’,’A’,’a’,’x’];
-arr[arr.length] = ‘x’;	//@end		[‘1’,’A’,’a’,’x’];
-arr.unshift(‘x’);			//@beginning	[‘x’,‘1’,’A’,’a’];
-//b.DELETING
-arr.pop();			//@end		[‘1’,’A’];
-arr.shift();			//@beginning	[’A’,’a’];
+    let arr = new Array ();
+    let arr = [];
+    let arr = [‘1’,’A’,’a’];
+
+          //a.ADDING DATA	
+          arr.push(‘x’);			    //@end		    [‘1’,’A’,’a’,’x’];
+          arr[arr.length] = ‘x’;	//@end		    [‘1’,’A’,’a’,’x’];
+          arr.unshift(‘x’);			  //@beginning	[‘x’,‘1’,’A’,’a’];
+
+          //b.DELETING
+          arr.pop();			        //@end		    [‘1’,’A’];
+          arr.shift();			      //@beginning	[’A’,’a’];
 
 
 
